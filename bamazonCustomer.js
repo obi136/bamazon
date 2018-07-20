7753cfc96353efb2a5ee9415821513fd14202cdc
obi136 <obi136@gmail.com>
2018-07-20 15:21:22 -0400

    new commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var tempAmt = 0;
+var totalAmt = 0;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -30,75 +32,86 @@ function printItems() {
 function checkout() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
+        var choiceArray = [];
+        for (var i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].product_name);
+            
+        }
+        // console.log(choiceArray);
+        // return choiceArray;
         inquirer
             .prompt([{
-                name: "choice",
-                type: "rawlist",
-                choices: function() {
-                    var choiceArray = [];
-                    for (var i = 0; i < res.length; i++) {
-                        choiceArray.push(res[i].product_name);
-                    }
-                    return choiceArray;
+                    name: "choice",
+                    type: "list",
+                    choices: choiceArray,
+                    // function () {
+                    //     // var choiceArray = [];
+                    //     // for (var i = 0; i < res.length; i++) {
+                    //     //     choiceArray.push(res[i].product_name);
+                    //     //     console.log(choiceArray);
+                    //     // }
+                    //     // return choiceArray;
+                    // },
+                    message: "Please select an item."
                 },
-                message: "Please select an item."
-            },
-            {
-                name: "quantity",
-                type: "input",
-                message: "How many units of this item would you like to buy?"
-            },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many units of this item would you like to buy?"
+                },
 
-            {
-                type: "confirm",
-                message: "Add this to your cart:",
-                name: "confirm",
-                default: false
-            }
-        ])
-        .then(function(answer){
-            var chosenItem;
-            for (var i = 0; i < res.length; i++){
-                if(res[i].product_name === answer.choice) {
-                    chosenItem = res[i];
+                {
+                    type: "confirm",
+                    message: "Add this to your cart:",
+                    name: "confirm",
+                    default: false
                 }
-            }
-
-            if (chosenItem.stock_quantity < parseInt(answer.quantity)){
-                connection.query(
-                    "SELECT (stock_quantity - 1) FROM products WHERE ?",
-                    [
-                        {
-                            item_id: chosenItem.item_id
-                        }
-                    ],
-                    function(error) {
-                        if (error) throw error;
-                        console.log("Item Purchase Successful!");
+            ])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.choice) {
+                        chosenItem = res[i];
                     }
-                );
-                connection.query(
-                    "SELECT (price * ?) FROM products WHERE ?",
-                    [
-                        {
-                           stock_quantity: answer.quantity     
-                        },
+                }
 
-                        {
+                if (chosenItem.stock_quantity >= parseInt(answer.quantity)) {
+
+                    tempAmt = chosenItem.price * answer.quantity;
+                    totalAmt = totalAmt + tempAmt;
+                    connection.query(
+                        // "SELECT (stock_quantity - " + answer.quantity + ") FROM products WHERE ?"
+                        "UPDATE products SET stock_quantity = " +  (chosenItem.stock_quantity - answer.quantity) + " WHERE ?", [{
                             item_id: chosenItem.item_id
+                        }],
+                        function (error) {
+                            if (error) throw error;
+                            console.log("Item Purchase Successful!");
                         }
-                    ],
-                    function(error) {
-                        if (error) throw error;
-                        console.log("All set!");
-                    }
-                );
-            }
-            else {
-                console.log("The product you requested has been sold out");
-                checkout();
-            }
-        });
+                    );
+                    connection.query(
+                        "SELECT (price * ?) FROM products WHERE ?", [{
+                                stock_quantity: answer.quantity
+                            },
+
+                            {
+                                item_id: chosenItem.item_id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw error;
+                            console.log("All set!");
+                            console.log("Your Total is: $" + totalAmt);
+                            totalAmt = 0;
+                            tempAmt = 0;
+                            checkout();
+                        }
+                    );
+                } else {
+                    console.log("The product you requested has been sold out");
+                    checkout();
+                }
+            });
     });
 
 }
